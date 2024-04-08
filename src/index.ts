68f2db9829e501be6003b7d721b8fd18ef246d8c
dannydaniil <danny.daniil@uniswap.org>
2024-04-07 22:41:46 -0400

    with simple index file

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,5 @@
-console.log('Try npm run lint/fix!');
-
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
-
-const trailing = 'Semicolon';
-
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
-  }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
+export function run() {
+  console.log('Hello, world!');
 }
-// TODO: more examples
+
+run();
