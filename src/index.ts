de88f6dbc128a4ca642933b5b2989ef9f6ba96e6
dannydaniil <danny.daniil@uniswap.org>
2024-04-07 22:46:58 -0400

    with key and new pkgs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+const DATA_INDEXER_API_KEY = process.env.DATA_INDEXER_API_KEY;
+const addresses = ['A2Qaj4jjUkaMvJ8iyWQUvdgYHKWgDqh6iQjUAb9tvHzH'];
+
 export function run() {
   console.log('Hello, world!');
 }
