1b82203d6d65790fec48349d3334c6843ed0c5ad
dannydaniil <danny.daniil@uniswap.org>
2024-04-08 10:52:40 -0400

    update name

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 const DATA_INDEXER_API_KEY = process.env.DATA_INDEXER_API_KEY;
+const DATA_INDEXER_API_URL = process.env.DATA_INDEXER_API_URL;
+
 const addresses = ['A2Qaj4jjUkaMvJ8iyWQUvdgYHKWgDqh6iQjUAb9tvHzH'];
 
 export function run() {
-  console.log('Hello, world!');
+  for (const address of addresses) {
+    console.log(`Processing address: ${address}`);
+  }
 }
 
 run();
